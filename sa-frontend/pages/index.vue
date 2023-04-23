7635ce9942ae860720007a1b00b20e4f6c3c3d27
nasarow <nasarow@epigno.jp>
2023-04-23 15:20:46 +0900

    [frontend] sensors: Adds new page folder, sensors page, and types
    WIP
    
    Signed-off-by: nasarow <nasarow@epigno.jp>

--- sa-frontend/pages/index.vue
+++ sa-frontend/pages/index.vue
@@ -1,11 +1,6 @@
 <template lang="pug">
 div
     h1 Hello
-    h2 Filters!
-    span(v-for="filter in store.filtersList") {{ filter }},
 </template>
 <script setup lang="ts">
-import { useFiltersStore } from '~/stores/filters'
-
-const store = useFiltersStore()
 </script>
--- sa-frontend/stores/sensors.ts
+++ sa-frontend/stores/sensors.ts
@@ -1,14 +1,15 @@
 import { defineStore } from 'pinia'
-import { Sensors } from '@/pages/sensors/composables/useSensors'
+import { Sensors } from '~/composables/useSensors'
 
-export const useSensorStore = defineStore('sensorStore', () => {
+export const useSensorStore = defineStore('sensorsStore', () => {
     const sensorList = ref<Sensors>({
         "@iot.count": 0,
         "value": []
     })
 
     const getSensors = async() => {
-        const sensorData:Sensors = await $fetch('api/v1.0/Sensors')
+        const sensorData:Sensors = await $fetch('http://localhost:8000/api/v1.0/Sensors')
+        console.log('sensor datas', sensorData)
 
         sensorList.value["@iot.count"] = sensorData["@iot.count"]
         sensorList.value.value.push(...sensorData.value)
